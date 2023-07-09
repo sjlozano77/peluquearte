d34bb0074436f87a075765b86ef79624fee41ca3
ezvel <lvelezezequiel7@gmail.com>
2023-07-09 02:20:27 -0300

    Arregla bug del menú de hamburgueza
    Lo que pasaba era que al agregarle la clase para que muestre el menú, al hacer click en el menú de hamburgueza para que se quite
    el menú, al intentar agrandar la pantalla a desktop por ejemplo, no mostraba el menú. Por este motivo el programa al capturar
    el event de resizear la pantall quita dinámicamente ese efecto para que se muestre el menú

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -4,3 +4,9 @@ const $menu = document.getElementById("menu");
 $toggleMenu.addEventListener("click", () => {
     $menu.classList.toggle("menu--show");
 })
+
+window.addEventListener("resize", () => {
+    if ($menu.classList.contains("menu--show")) {
+        $menu.classList.remove("menu--show");
+    }
+})
