b5537b9012bca99bc9c4e4bada3d6bb6ef26e7d9
ezvel <lvelezezequiel7@gmail.com>
2023-07-10 05:43:48 -0300

    Unir repositorios y actualizar proyecto

--- pages/administradores/consultar_turnos.php
+++ pages/administradores/consultar_turnos.php
@@ -95,6 +95,66 @@
         </ul>
     </nav>
     <main class="main">
+        <header class="main-header">
+            <h2 class="main-header-titulo">Mis turnos</h2>
+        </header>
+        <section class="main-contenido">
+            <div class="main-contenido-tabla">
+                <?php 
+                    // Ejecutar la consulta de reservas
+                    $sql = "SELECT reservas.Id_Reserva, reservas.DNI, reservas.Nombre, reservas.Apellido, reservas.Dia, reservas.Hora, empleado.Nombre, empleado.Apellido, reservas.Estado FROM reservas inner join empleado on reservas.Id_empleado = empleado.Id_empleado";
+                    $resultado = mysqli_query($conexion, $sql) or die("Error al ejecutar la consulta");
+
+                    // Mostrar la consulta en una tabla
+                    echo "<table class='tabla'>";
+                    echo "<tr class='tabla__encabezado'><th class='tabla__celda'>Reserva</th><th class='tabla__celda'>DNI</th><th class='tabla__celda'>Nombre</th><th class='tabla__celda'>Dia</th><th class='tabla__celda'>Hora</th><th class='tabla__celda'>Peluquero/a</th><th class='tabla__celda'>Estado</th><th class='tabla__celda'>Acciones</th></tr>";
+                    $contador = 0;
+                    while ($fila = mysqli_fetch_row($resultado)) {
+                        if($contador % 2 == 0) {
+                            echo "<tr class='tabla__fila tabla__fila--par'>";
+                            echo "<td class='tabla__celda tabla__celda--negrita'>" . $fila[0] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[1] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[2] . " " . $fila[3] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[4] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[5] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[6] . " " .  $fila[7] . "</td>";
+                            if($fila[8] == "Confirmado") {
+                                echo "<td class='tabla__celda'>" . "<div class='estado estado--confirmado'></div>"  . "</td>";
+                            } else if ($fila[8] == "reservado") {
+                                echo "<td class='tabla__celda'>" . "<div class='estado estado--reservado'></div>"  . "</td>";
+                            } else { //cancelado
+                                echo "<td class='tabla__celda'>" . "<div class='estado estado--cancelado'></div>" . "</td>";
+                            }
+                            echo "<td class='tabla__celda tabla__celda--display-flex-row'>" . "<form action='bdd/actualizar_estado_a_confirmado.php' method='get'><textarea class='oculto' name='id_reserva'>$fila[0]</textarea><button class='acciones acciones--confirmar' type='submit'><img src='assets/iconos/confirmar.svg'></button></form>" . "<form action='bdd/actualizar_estado_a_cancelado.php' method='get'><textarea class='oculto' name='id_reserva'>$fila[0]</textarea><button class='acciones acciones--cancelar' type='submit'><img src='assets/iconos/cancelar.svg'></button></form>". "</td>";
+                            echo "</tr>";
+                        } else {
+                            echo "<tr class='tabla__fila tabla__fila--impar'>";
+                            echo "<td class='tabla__celda tabla__celda--negrita'>" . $fila[0] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[1] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[2] . " " . $fila[3] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[4] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[5] . "</td>";
+                            echo "<td class='tabla__celda'>" . $fila[6] . " " .  $fila[7] . "</td>";
+                            if($fila[8] == "Confirmado") {
+                                echo "<td class='tabla__celda'>" . "<div class='estado estado--confirmado'></div>"  . "</td>";
+                            } else if ($fila[8] == "reservado") {
+                                echo "<td class='tabla__celda'>" . "<div class='estado estado--reservado'></div>"  . "</td>";
+                            } else { //cancelado
+                                echo "<td class='tabla__celda'>" . "<div class='estado estado--cancelado'></div>" . "</td>";
+                            }
+                            echo "<td class='tabla__celda tabla__celda--display-flex-row'>" . "<form action='bdd/actualizar_estado_a_confirmado.php' method='get'><textarea class='oculto' name='id_reserva'>$fila[0]</textarea><button class='acciones acciones--confirmar' type='submit'><img src='assets/iconos/confirmar.svg'></button></form>" . "<form action='bdd/actualizar_estado_a_cancelado.php' method='get'><textarea class='oculto' name='id_reserva'>$fila[0]</textarea><button class='acciones acciones--cancelar' type='submit'><img src='assets/iconos/cancelar.svg'></button></form>". "</td>";
+                            echo "</tr>";
+                        }
+
+                        $contador++;
+                    }
+                    echo "</table>";
+
+                    // Cerrar la conexión
+                    mysqli_close($conexion);
+                ?>
+            </div>
+        </section>
     </main>
     <script type="text/javascript" src="../../js/formulario.js"></script>
     <script type="text/javascript" src="../../js/toggle-menu.js"></script>
